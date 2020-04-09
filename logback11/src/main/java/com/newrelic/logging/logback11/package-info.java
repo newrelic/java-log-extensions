678f6d815c300429a6e59dbdd09e63525ae030e6
Jeff Alder <49817386+jeffalder@users.noreply.github.com>
2020-04-09 11:03:02 -0700

    Fix javadoc class references.

--- logback11/src/main/java/com/newrelic/logging/logback11/package-info.java
+++ logback11/src/main/java/com/newrelic/logging/logback11/package-info.java
@@ -1,8 +1,8 @@
 /**
  * The New Relic Log plugin for Logback 1.1.x.
  *
- * Use the public classes in your logging config xml. The {@link com.newrelic.logging.logback.NewRelicEncoder} must be
- * used as an encoder for the New Relic JSON format. The {@link com.newrelic.logging.logback.NewRelicAsyncAppender} must
+ * Use the public classes in your logging config xml. The {@link com.newrelic.logging.logback11.NewRelicEncoder} must be
+ * used as an encoder for the New Relic JSON format. The {@link com.newrelic.logging.logback11.NewRelicAsyncAppender} must
  * be used to wrap the appender so that the encoder can capture the trace and span information at the time of the
  * log message.
  *
