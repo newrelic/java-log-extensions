dba25bfba9675f36ec80eb1e8c669b3a3d714869
Jeff Alder <49817386+jeffalder@users.noreply.github.com>
2020-07-20 14:56:45 -0700

    Remove Gradle module metadata

--- gradle/publish.gradle.kts
+++ gradle/publish.gradle.kts
@@ -54,3 +54,8 @@ configure<PublishingExtension> {
         }
     }
 }
+
+// This makes it difficult to use modern Java and produce usable output.
+tasks.withType<GenerateModuleMetadata> {
+    enabled = false
+}
