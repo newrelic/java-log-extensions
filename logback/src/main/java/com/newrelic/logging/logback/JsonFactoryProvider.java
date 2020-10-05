cb71c26c66b5eaf495b2db0d9fcc36463d9c10ad
gstoupis <gstoupis@gmail.com>
2020-10-06 00:12:35 +0300

    fix: avoid race condition in JsonFactoryProvider

--- logback/src/main/java/com/newrelic/logging/logback/JsonFactoryProvider.java
+++ logback/src/main/java/com/newrelic/logging/logback/JsonFactoryProvider.java
@@ -3,12 +3,9 @@ package com.newrelic.logging.logback;
 import com.fasterxml.jackson.core.JsonFactory;
 
 public class JsonFactoryProvider {
-    private static JsonFactory jsonFactory;
+    private static final JsonFactory jsonFactory = new JsonFactory();
 
     static JsonFactory getInstance() {
-        if (jsonFactory == null) {
-            jsonFactory = new JsonFactory();
-        }
         return jsonFactory;
     }
 
