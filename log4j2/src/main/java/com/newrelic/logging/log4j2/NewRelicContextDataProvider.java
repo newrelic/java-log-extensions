daa74c0255afeb83b7d6a95844a455cea73dfaee
Todd W Crone <twcrone@gmail.com>
2021-11-03 16:44:42 -0400

    Temp logging to allow customer troubleshoot

--- log4j2/src/main/java/com/newrelic/logging/log4j2/NewRelicContextDataProvider.java
+++ log4j2/src/main/java/com/newrelic/logging/log4j2/NewRelicContextDataProvider.java
@@ -9,7 +9,10 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 import java.util.function.Supplier;
+import java.util.logging.Level;
+import java.util.stream.Collectors;
 
+import com.newrelic.api.agent.Logger;
 import org.apache.logging.log4j.core.util.ContextDataProvider;
 
 import com.newrelic.api.agent.Agent;
@@ -26,16 +29,28 @@ public class NewRelicContextDataProvider implements ContextDataProvider {
 
     @Override
     public Map<String, String> supplyContextData() {
+        Logger logger = agentSupplier.get().getLogger();
+        logger.log(Level.INFO, "LogExtension:Supply context data");
         Map<String, String> meta = agentSupplier.get().getLinkingMetadata();
         if (meta != null && meta.size() > 0) {
+            logger.log(Level.INFO, "LogExtension:Found metadata with {0} entries", meta.size());
             Map<String, String> map = new HashMap<>();
             Set<Map.Entry<String, String>> metaSet = meta.entrySet();
             for (Map.Entry<String, String> entry : metaSet) {
                 map.put(NEW_RELIC_PREFIX + entry.getKey(), entry.getValue());
             }
+            logger.log(Level.INFO, "LogExtension:LinkingMetadata" + mapToString(map));
             return map;
         } else {
+            logger.log(Level.INFO, "LogExtension:No linking metadata received from agent");
             return EMPTY_MAP;
         }
+    }
+
+    // debugging purposes only
+    private static String mapToString(Map<String, String> map) {
+        return "[" + map.entrySet().stream()
+                .map(entry -> entry.getKey() + "=" + entry.getValue())
+                .collect(Collectors.joining(",")) + "]";
     }
 }
