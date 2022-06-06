cfa29293696fdd99b4e64ab4c4d7db1751270512
Dan Gibson <daniel.gibson@news.co.uk>
2022-06-06 15:22:48 +0100

    Prevent the includeJar in logback adding transitives in forwarder in a fatjar style

--- forwarder/build.gradle.kts
+++ forwarder/build.gradle.kts
@@ -9,10 +9,15 @@ repositories {
     mavenCentral()
 }
 
+// Prevent dependencies in this subproject from getting fat-jar-ed into projects using includeInJar
+val compileAndTestOnly: Configuration by configurations.creating
+configurations["compileOnly"].extendsFrom(compileAndTestOnly)
+configurations["testImplementation"].extendsFrom(compileAndTestOnly)
+
 dependencies {
-    implementation("com.newrelic.agent.java:newrelic-api:7.6.0")
-    implementation("com.newrelic.telemetry:telemetry-core:0.13.2")
-    implementation("com.newrelic.telemetry:telemetry-http-okhttp:0.13.2")
+    compileAndTestOnly("com.newrelic.agent.java:newrelic-api:7.6.0")
+    compileAndTestOnly("com.newrelic.telemetry:telemetry-core:0.13.2")
+    compileAndTestOnly("com.newrelic.telemetry:telemetry-http-okhttp:0.13.2")
 
     testImplementation("org.junit.jupiter:junit-jupiter:5.6.2")
     testImplementation("org.mockito:mockito-core:3.4.4")
