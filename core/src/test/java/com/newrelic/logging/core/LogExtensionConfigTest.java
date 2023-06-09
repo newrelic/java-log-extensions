d1065ae377217ad4f337b17503b77a84851c5d0c
jasonjkeller <jasonjkeller@gmail.com>
2023-06-08 17:26:01 -0700

    Add copyright header

--- core/src/test/java/com/newrelic/logging/core/LogExtensionConfigTest.java
+++ core/src/test/java/com/newrelic/logging/core/LogExtensionConfigTest.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright 2023 New Relic Corporation. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package com.newrelic.logging.core;
 
 import org.junit.jupiter.api.Test;
