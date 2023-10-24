9fdb26ce0df299388a2365dbaae836f6534d9b75
Kate Anderson <kateanderson@newrelic.com>
2023-10-24 13:03:23 -0700

    Update logback-app logback dependencies to use latest version

--- examples/logback-app/build.gradle.kts
+++ examples/logback-app/build.gradle.kts
@@ -12,17 +12,17 @@ repositories {
 
 dependencies {
     implementation(project(":logback"))
-    implementation("ch.qos.logback:logback-core:1.2.3")
-    implementation("ch.qos.logback:logback-classic:1.2.3")
+    implementation("ch.qos.logback:logback-core:1.4.11")
+    implementation("ch.qos.logback:logback-classic:1.4.11")
     implementation("com.fasterxml.jackson.core:jackson-core:2.11.1")
 
-    implementation("com.newrelic.agent.java:newrelic-api:7.6.0")
+    implementation("com.newrelic.agent.java:newrelic-api:8.6.0")
 }
 
 
 configure<JavaPluginConvention> {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
 }
 
 application {
