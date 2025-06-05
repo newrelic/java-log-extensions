f6e95b1559931a6fb982220fc465c9fd1f073247
edeleon <edeleon@newrelic.com>
2025-06-05 03:44:14 -0700

    refactor and clean up

--- logback13/src/main/java/com/newrelic/logging/logback13/NewRelicAsyncAppender.java
+++ logback13/src/main/java/com/newrelic/logging/logback13/NewRelicAsyncAppender.java
@@ -39,70 +39,42 @@ import static com.newrelic.logging.core.LogExtensionConfig.CONTEXT_PREFIX;
 public class NewRelicAsyncAppender extends AsyncAppender {
     public static final String NEW_RELIC_PREFIX = "NewRelic:";
 
-    // required for logback-1.3.x compatibility
-    @Override
-    public void start() {
-        super.start();
-    }
-
     @Override
     protected void preprocess(ILoggingEvent eventObject) {
         eventObject.prepareForDeferredProcessing();
-//        Map<String, String> linkingMetadata = agentSupplier.get().getLinkingMetadata();
-//        Map<String, String> mdcCopy = MDC.getMDCAdapter().getCopyOfContextMap();
-//        Map<String, String> combinedContextMap = new HashMap<>();
-//        ILoggingEvent wrappedEvent;
-//        System.out.println("[NewRelicAsyncAppender.preprocess] linkingMetadata = agentSupplier.get().getLinkingMetadata() : " + linkingMetadata.entrySet());
-//        System.out.println("[NewRelicAsyncAppender.preprocess] mdcCopy = eventObject.getMDCPropertyMap() : " + mdcCopy.entrySet());
-//
-//        if (!isNoOpMDC) {
-//            combinedContextMap.putAll(linkingMetadata);
-//
-//            for (Map.Entry<String, String> entry : mdcCopy.entrySet()) {
-//                combinedContextMap.put(CONTEXT_PREFIX + entry.getKey(), entry.getValue());
-//            }
-//            System.out.println("[NewRelicAsyncAppender.preprocess] combinedContextMap after adding CONTEXT_PREFIX: " + combinedContextMap);
-//
-//            wrappedEvent = new CustomLoggingEventWrapper(eventObject, combinedContextMap);
-//            super.preprocess(wrappedEvent);
-//        }
-//
-//        /*
-//         * In logback-1.3.x, calling eventObject.getMDCPropertyMap() returns an immutable map.
-//         * To add New Relic linking metadata to the event, we need to pull the existing ContextMap and set it as the MDCPropertyMap.
-//         * This allows us to maintain compatibility with logback-1.3.x while still supporting the New Relic linking metadata
-//         * in the event object.
-//         */
-//        if (isNoOpMDC) {
-//            for (Map.Entry<String, String> entry : linkingMetadata.entrySet()) {
-//                combinedContextMap.put(NEW_RELIC_PREFIX + entry.getKey(), entry.getValue());
-//            }
-//            wrappedEvent = new CustomLoggingEventWrapper(eventObject, combinedContextMap);
-//            super.preprocess(wrappedEvent);
-//        }
     }
 
     @Override
     protected void append(ILoggingEvent eventObject) {
+        ILoggingEvent wrappedEvent;
         if (!isStarted()) {
             return;
         }
 
-        Map<String, String> baseMdc = MDC.getMDCAdapter().getCopyOfContextMap();
         Map<String, String> combinedContextMap = new HashMap<>();
 
-        for (Map.Entry<String, String> entry : baseMdc.entrySet()) {
-            combinedContextMap.put(CONTEXT_PREFIX + entry.getKey(), entry.getValue());
+        Map<String, String> copyMdc = MDC.getMDCAdapter().getCopyOfContextMap();
+        if (copyMdc != null) {
+            for (Map.Entry<String, String> entry : copyMdc.entrySet()) {
+                if (entry.getKey() != null && entry.getValue() != null) {
+                    combinedContextMap.put(CONTEXT_PREFIX + entry.getKey(), entry.getValue());
+                }
+            }
         }
 
         Map<String, String> linkingMetadata = agentSupplier.get().getLinkingMetadata();
-        combinedContextMap.putAll(linkingMetadata);
-        System.out.println("[NewRelicAsyncAppender.append] combinedContextMap: " + combinedContextMap.entrySet());
+        for (Map.Entry<String, String> entry : linkingMetadata.entrySet()) {
+            combinedContextMap.put(NEW_RELIC_PREFIX + entry.getKey(), entry.getValue());
+        }
 
-        ILoggingEvent wrappedEvent = new CustomLoggingEventWrapper(eventObject, combinedContextMap);
-
-        System.out.println("[NewRelicAsyncAppender.append] wrappedEvent: " + wrappedEvent.getMDCPropertyMap().entrySet());
-
+        if (!isNoOpMDC) {
+            for (Map.Entry<String, String> entry : linkingMetadata.entrySet()) {
+                MDC.put(NEW_RELIC_PREFIX + entry.getKey(), entry.getValue());
+            }
+            wrappedEvent = eventObject;
+        } else {
+            wrappedEvent = new CustomLoggingEventWrapper(eventObject, combinedContextMap);
+        }
         super.append(wrappedEvent);
     }
 
