61ab0c069b9c1cc4fe62b0ae6baade80e7b0d660
edeleon <edeleon@newrelic.com>
2025-06-05 05:52:50 -0700

    developing new testing suite

--- logback13/src/test/java/NewRelicLogback13Tests.java
+++ logback13/src/test/java/NewRelicLogback13Tests.java
@@ -1,6 +1,5 @@
 import ch.qos.logback.classic.LoggerContext;
 import ch.qos.logback.classic.spi.ILoggingEvent;
-import ch.qos.logback.classic.spi.LoggingEvent;
 import ch.qos.logback.core.ConsoleAppender;
 import ch.qos.logback.core.read.ListAppender;
 import com.newrelic.api.agent.Agent;
